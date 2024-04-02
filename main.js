4f5a8d332b127b3a08a16ba886b3330be2cc756a
Andrew Korobko <74743351+primovocatus@users.noreply.github.com>
2024-04-02 21:10:01 +0300

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const currentyear = new Date().getFullYear();
         const seconds = document.getElementById("seconds");
         const countDown = document.getElementById("countdown");
         const currentYear = new Date().getFullYear();
-        const newYearTime = new Date(`March 25 ${currentYear} 10:00:00`);
+        const newYearTime = new Date(`April 4 ${currentYear} 14:00:00`);
 
         function play() {
             audio.play();
